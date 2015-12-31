3cc632755025b0dc771fe76ca7cf94b5c29db22a
Mike Nagle <me@mikenagle.me>
2015-12-31 15:24:25 +0000

    more questions

--- js/background.js
+++
@@ -1,5 +0,0 @@
-function changeImage() {
-    var bgImage = ((Math.floor(Math.random() * 3))+1)+ ".jpg";
-    var yourBackground = document.getElementById('hero');
-    yourBackground.style.backgroundImage = 'url(../img/backgrounds/' + bgImage + ')' ;
-}
--- js/questions_data.js
+++ js/questions_data.js
@@ -1,47 +1,252 @@
 var questions = [
     {
         id: 1,
-        option1: 'Be forced to wear wet socks for the rest of your life?',
+        option1: 'Be forced to wear wet socks for the rest of your life',
         option2: 'Be allowed to wash your hair only once a year?'
     },   
     {
         id: 2,
-        option1: 'Have a finger as a tongue?',
+        option1: 'Have a finger as a tongue',
         option2: 'Have tongues as fingers?'
     },   
     {
         id: 3,
-        option1: 'Wear someone else\'s dirty underwear?',
+        option1: 'Wear someone else\'s dirty underwear',
         option2: 'Use someone else\'s toothbrush?'
     },   
     {
         id: 4,
-        option1: 'Shit bricks?',
+        option1: 'Shit bricks',
         option2: 'Puke slugs?'
     },   
     {
         id: 5,
-        option1: 'Pee yourself in public once a week?',
+        option1: 'Pee yourself in public once a week',
         option2: 'Shit yourself in private daily?'
     },   
     {
         id: 6,
-        option1: 'Always have to tell the truth?',
+        option1: 'Always have to tell the truth',
         option2: 'Always lie?'
     },
     {
         id: 7,
-        option1: 'Never have internet access again?',
+        option1: 'Never have internet access again',
         option2: 'Never be allowed to take an airplane anywhere again?'
     },   
     {
         id: 8,
-        option1: 'Have your body found in a pile of sex toys?',
+        option1: 'Have your body found in a pile of sex toys',
         option2: 'Have everyone you know see your browsing history?'
     },   
     {
         id: 9,
         option1: 'Be in the real-life version of \"American Horror Story\"',
         option2: 'Be in the real-life version of \"The Walking Dead\"?'
+    },
+    {
+        id: 10,
+        option1: 'Only be able to whisper everything',
+        option2: 'Only be able to shout everything?'
     },   
+    {
+        id: 11,
+        option1: 'Lick a London pavement',
+        option2: 'Put your tongue inside a stranger\'s nostril?'
+    },   
+    {
+        id: 12,
+        option1: 'Fight a dog-sized cockroach',
+        option2: 'Fight a horse-sized rat'
+    },   
+    {
+        id: 13,
+        option1: 'Immerse yourself in a bathtub of spiders',
+        option2: 'A bathtub of tobacco spit?'
+    },   
+    {
+        id: 14,
+        option1: 'Have a photographic memory',
+        option2: 'Be able to completely forget anything you wanted?'
+    },   
+    {
+        id: 15,
+        option1: 'Give up your five favourite foods forever',
+        option2: 'Give up sex forever?'
+    },   
+    {
+        id: 16,
+        option1: 'Wear an eyepatch',
+        option2: 'Have a peg leg?'
+    },   
+    {
+        id: 17,
+        option1: 'Be blind',
+        option2: 'Be dumb?'
+    },   
+    {
+        id: 18,
+        option1: 'Have narcolepsy',
+        option2: 'Have chronic insomnia?'
+    },   
+    {
+        id: 19,
+        option1: 'Have no elbows',
+        option2: 'Have no knees
+    },   
+    {
+        id: 20,
+        option1: 'Be 1 metre taller',
+        option2: 'Be 1 metre shorter'
+    },   
+    {
+        id: 21,
+        option1: 'Be an elephant',
+        option2: 'Be a giraffe'
+    },   
+    {
+        id: 22,
+        option1: 'Live one life that lasts 1,000 years',
+        option2: 'live 10 lives that last 100 years each?'
+    },   
+    {
+        id: 23,
+        option1: 'use eye drops made of vinegar',
+        option2: 'toilet paper made from sandpaper?'
+    },   
+    {
+        id: 24,
+        option1: 'experience a sharp pain in your side each time someone says your name',
+        option2: 'have a bell sound each time you are aroused?'
+    },   
+    {
+        id: 25,
+        option1: 'have a large 10 inch long belly button that swayed to music',
+        option2: ' have accordions for legs?'
+    },   
+    {
+        id: 26,
+        option1: 'have to sneeze but not be able to',
+        option2: 'have something stuck in your eye for an entire year?'
+    },   
+    {
+        id: 27,
+        option1: 'have a dragon',
+        option2: 'Be a dragon?'
+    },   
+    {
+        id: 28,
+        option1: 'secretly have sex with a goat',
+        option2: 'have everyone think you had sex with a goat even though you didn’t?'
+    },   
+    {
+        id: 29,
+        option1: 'have hair nowhere on your body',
+        option2: 'be very hairy all over and not be able to shave?'
+    },   
+    {
+        id: 30,
+        option1: 'never be able to speak again',
+        option2: 'always have to say everything that is on your mind?'
+    },   
+    {
+        id: 31,
+        option1: 'be able to read',
+        option2: 'be able to read minds (but be illiterate)?'
+    },   
+    {
+        id: 32,
+        option1: 'be able to speak fluently every language in the world',
+        option2: 'be the best in the world at something of your choosing?'
+    },   
+    {
+        id: 33,
+        option1: 'be in prison for five years in solitary confinement',
+        option2: 'not ever go to prison and not ever become rich?'
+    },   
+//    {
+//        id: 34,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },   
+//    {
+//        id: 35,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },   
+//    {
+//        id: 36,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },   
+//    {
+//        id: 37,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },
+//    {
+//        id: 38,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },
+//    {
+//        id: 39,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },
+//    {
+//        id: 40,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },
+//    {
+//        id: 41,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },
+//    {
+//        id: 42,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },
+//    {
+//        id: 43,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },
+//    {
+//        id: 44,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },
+//    {
+//        id: 45,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },
+//    {
+//        id: 46,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },
+//    {
+//        id: 47,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },
+//    {
+//        id: 48,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },
+//    {
+//        id: 49,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },
+//    {
+//        id: 50,
+//        option1: 'Be in the real-life version of \"American Horror Story\"',
+//        option2: 'Be in the real-life version of \"The Walking Dead\"?'
+//    },
 ]
